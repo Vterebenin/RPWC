13f051d2f606db6e22233efd0a331cee698f0267
Vterebenin <niceserver11@mail.ru>
2018-10-07 23:56:56 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(email: ENV['CANAPE_ADMIN_EMAIL'], password: ENV['CANAPE_ADMIN_PASSWORD'])
