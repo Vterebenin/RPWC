952342cb54e771941e15c830044fc11ff8e0a2d2
Vterebenin <niceserver11@mail.ru>
2018-10-07 23:46:36 +0300

    fast deploy

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,5 @@ class Admin < ApplicationRecord
 	has_many :resources
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :categories
   devise_for :admins
-  root 'static_pages#home'
+  root 'resources#index'
+  get 'home', to: 'static_pages#home'
   get 'cssrule', to: 'static_pages#cssrule'
   get 'static_pages/test'
   resources :resources
+  devise_scope :admin do
+		get '/admin' => 'devise/sessions#new'
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
